b43e54867093ef8fa460abaf9e03f419545e9ab8
Ramadoss Mohan <ramadoss.mohan@gmail.com>
2023-03-22 20:14:14 +0800

    Update url.ps1

--- packer-gcp/gcp/url.ps1
+++ packer-gcp/gcp/url.ps1
@@ -8,15 +8,20 @@ write-host "(host) Running User Data Script"
 #net user ${var.packer_username} \"${var.packer_user_password}\" /add /y & wmic UserAccount where Name=\"${var.packer_username}\" set PasswordExpires=False & net localgroup administrators ${var.packer_username} /add 
 
 #add packer user 
-net user packer_user  "=|i3FdD3,{C<:^n" /add /y
+#net user packer_user  "=|i3FdD3,{C<:^n" /add /y
 
-Set-LocalUser -Name packer_user -PasswordNeverExpires 0
-
-
-net localgroup administrators packer_user /add 
-
+#Set-LocalUser -Name packer_user -PasswordNeverExpires 0
+#
+#net localgroup administrators packer_user /add 
 
 
+# Set administrator password
+#net user Administrator cred
+#wmic useraccount where "name='Administrator'" set PasswordExpires=FALSE
+ 
+#set mrbar password and add it to the adminstrator
+#net user /add packeradmin cred
+#net localgroup administrators packeradmin /add
  
 
 ##Create install folder and use for Gpo
@@ -32,14 +37,20 @@ Start-Transcript -Path "C:\Install\Posh_transcript.txt" -NoClobber
 $installPath = "C:\install"
 New-Item -ItemType Directory -Path $installPath -Force
 
+
+Write-Host "Downloading OpenSSH binary"
+Add-Content C:\Install\gcp_soi.log "[$(Get-Date)] => Installing OpenSSH`n"
+
+
 # Define the URL to the OpenSSH installer
-$installerUrl = "https://github.com/PowerShell/Win32-OpenSSH/releases/download/v9.2.0.0p1-Beta/OpenSSH-Win64-v9.2.0.0.msi"
+#$installerUrl = "https://storage.cloud.google.com/abctest2/ssh/OpenSSH-Win64-v9.2.0.0.msi"
+
 
 # Define the path to save the installer
-$installerPath = "C:\install\OpenSSH-Win64-v9.2.0.0.msi"
+#$installerPath = "C:\install\OpenSSH-Win64-v9.2.0.0.msi"
 
 # Download the OpenSSH installer from the URL
-Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+#Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
 
 # Install OpenSSH
 Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart" -Wait
@@ -47,4 +58,26 @@ Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart
 # Delete the installer file
 #Remove-Item $installerPath
 
+
+# Set variables
+$OpenSSHName = "OpenSSH-Win64-v9.2.0.0.msi"
+$OpenSSHFile =   "gs://abctest2/ssh/OpenSSH-Win64-v9.2.0.0.msi"
+# Define the path to save the installer
+$installerPath = "C:\install\OpenSSH-Win64-v9.2.0.0.msi"
+
+
+# Download  OpenSSH installer
+Start-Process -FilePath "gsutil.cmd" -ArgumentList "cp", $OpenSSHFile, "C:\install\" -Wait
+
+
+# Install OpenSSH
+Start-Process msiexec.exe -ArgumentList "/i `"$installerPath`" /quiet /norestart" -Wait
+
+
+# Delete the installer file
+#Remove-Item $installerPath
+
+
 Stop-Transcript
+
+
